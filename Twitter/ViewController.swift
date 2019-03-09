d9fb4b455a242f868d70e95b1a321c702f39d3a4
Mamadou A. Balde <baldeadama89@gmail.com>
2019-03-09 11:02:25 -0500

    Initial Commit

---
+++ Twitter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Twitter
+//
+//  Created by Mamadou A. Balde on 3/9/19.
+//  Copyright © 2019 MamadouABalde. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
